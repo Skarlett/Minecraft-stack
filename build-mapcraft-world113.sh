9783d52e92613d19a621b5c293e8641cf3cf3316
Adam <3759687+Skarlett@users.noreply.github.com>
2020-08-16 13:36:08 -0500

    Update build-mapcraft-world113.sh

--- build-mapcraft-world113.sh
+++ build-mapcraft-world113.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 ###################
 # Builds mapcrafter from source for modern minecraft support
+# Ubuntu 18.04 LTS
 
 apt install g++ cmake build-essential libturbojpeg libturbojpeg-dev libblkid-dev e2fslibs-dev libboost-all-dev libaudit-dev git -y
-git install https://github.com/mapcrafter/mapcrafter --branch world113
+git clone https://github.com/mapcrafter/mapcrafter --branch world113
 
 cd mapcrafter
 
